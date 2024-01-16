04ffdae4332d83a7f145d7a83367ac5c2242b372
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-01-16 22:52:56 +1300

    Create mainscript.lua

---
+++ mainscript.lua
@@ -0,0 +1,12 @@
+-- INSTRUCTIONS
+-- 1. Execute the script
+-- 2. Find the keyboard GUI and click the search bar (only for mobile)
+-- 3. Type "z" (only for mobile)
+-- 4. Insert "Z" (only for mobile)
+-- 5. Close the keyboard GUI
+-- 6. Use flowing water on someone
+-- 7. During flowing water, press the Z button (or Z key if ur on computer)
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/JohnyGamingLUA/DomainExpansion/main/obfuscated.lua'))()
+
+-- Script by JohnyGaming

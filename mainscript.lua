4c2495a29d36546253d79df8e2f27d2f82ea6222
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-02-14 21:45:17 +1300

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,11 +1,15 @@
 -- INSTRUCTIONS
 -- 1. Execute the script
 -- 2. Find the keyboard GUI and click the search bar (only for mobile)
--- 3. Type "z" (only for mobile)
--- 4. Insert "Z" (only for mobile)
--- 5. Close the keyboard GUI
--- 6. Use flowing water on someone
--- 7. During flowing water, press the Z button (or Z key if ur on computer)
+-- 3. Type "X", then insert "X". Repeat the same for Y and Z. (only for mobile)
+-- 4. Close the keyboard GUI (only for mobile)
+-- 5. Use flowing water on someone
+-- 6. During flowing water, press the Z button (or Z key if ur on computer)
+
+-- DOMAINS
+-- X - Fist Room of Doom
+-- Y - Micro Atomic Maze
+-- Z - Baseplate
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/JohnyGamingLUA/DomainExpansion/main/obfuscated.lua'))()
 

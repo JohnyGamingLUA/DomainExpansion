044fb738c844ff54612f34c7f8df8ce173b252dd
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-02-16 22:01:28 +1300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,10 +1,12 @@
 -- INSTRUCTIONS
+-- MOBILE
 -- 1. Execute the script
--- 2. Find the keyboard GUI and click the search bar (only for mobile)
--- 3. Type "X", then insert "X". Repeat the same for Y and Z. (only for mobile)
--- 4. Close the keyboard GUI (only for mobile)
--- 5. Use flowing water on someone
--- 6. During flowing water, press the X, Y or Z button (or X, Y, or Z key if ur on computer)
+-- 2. Use flowing water on someone
+-- 3. Press one of the domain buttons on the upper left side of your screen  
+-- PC  
+-- 1. Execute the script
+-- 2. Use flowing water on someone
+-- 3. Press 5, 6 or 7 to expand your domain
 
 -- DOMAINS
 -- X - Fist Room of Doom

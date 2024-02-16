be6e64671a7a356540cd66b962330b56bfed72de
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-02-16 20:10:30 +1300

    Update mobilegui.lua

--- mobilegui.lua
+++ mobilegui.lua
@@ -23,7 +23,7 @@ TextButton.Position = UDim2.new(0.10999999978, 0, 0.00967741944, 0)
 TextButton.Size = UDim2.new(0, 116, 0, 39)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.Text = "FistRoomOfDoom"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.TextSize = 14.000
 
 TextButton_2.Parent = ScreenGui
@@ -35,7 +35,7 @@ TextButton_2.Position = UDim2.new(0.10999999978, 0, 0.075000003, 0)
 TextButton_2.Size = UDim2.new(0, 116, 0, 39)
 TextButton_2.Font = Enum.Font.SourceSans
 TextButton_2.Text = "MicroAtomicMaze"
-TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton_2.TextSize = 14.000
 
 TextButton_3.Parent = ScreenGui
@@ -47,7 +47,7 @@ TextButton_3.Position = UDim2.new(0.10999999978, 0, 0.142000005, 0)
 TextButton_3.Size = UDim2.new(0, 116, 0, 39)
 TextButton_3.Font = Enum.Font.SourceSans
 TextButton_3.Text = "Baseplate"
-TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton_3.TextSize = 14.000
 
 TextButton.MouseButton1Down:Connect(function()

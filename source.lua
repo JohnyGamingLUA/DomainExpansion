b526d35a5e2c47cedf7ec5b7e64143959da040ef
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-02-16 23:07:44 +1300

    Update source.lua

--- source.lua
+++ source.lua
@@ -20,5 +20,14 @@ if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and no
 elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
     print("PC user")
 end
-
+canspawngui = true
+while true do
+  if  canspawngui == true then
+	canspawngui = false
+	repeat task.wait() until game.Players.LocalPlayer.Character.Humanoid.Health == 0
+        wait(6)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/JohnyGamingLUA/DomainExpansion/main/mobilegui.lua'))()
+        canspawngui = true
+  end
+end
 -- Script by JohnyGaming
